3c0364278c1e8f36d5d8c5963f54ed2130f8734e
kdrag0n <dragon@khronodragon.com>
2021-01-02 14:01:58 +0800

    umi: disable I/O stats accounting by default
    While Android userspace (e.g. storaged) does use iostats via
    /proc/diskstats, init will explicitly enable iostats for the devices on
    which it is primarily used - sda and sdf. Avoid the 0.5-1% overhead for
    block devices that do not need it.
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- umi_system.sh
+++ umi_system.sh
@@ -25,3 +25,11 @@
 # OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
+
+# 禁用磁盘I/O统计 (UFS)
+echo "0" > /sys/block/sda/queue/iostats
+echo "0" > /sys/block/sdb/queue/iostats
+echo "0" > /sys/block/sdc/queue/iostats
+echo "0" > /sys/block/sdd/queue/iostats
+echo "0" > /sys/block/sde/queue/iostats
+echo "0" > /sys/block/sdf/queue/iostats
